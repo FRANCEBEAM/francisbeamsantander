18b8ef87a73b78da089f68ea5ba3165bab291444
Francis Beam Santander <santanderfrancisbeam@gmail.com>
2023-07-01 10:07:53 +0800

    tech stacks about section

--- src/components/PageTemplate.js
+++ src/components/PageTemplate.js
@@ -3,6 +3,71 @@
 export const aboutContent = [
     {
         parag1: "Hi, my name is Francis Beam Santander. I am fresh graduate of Bulacan State University - Sarmiento Campus, holding a Bachelor's degree in Information Technology . Throughout my academic journey, I have cultivated a deep passion for front-end development and UX/UI design.",
-        parag2: "I enjoy assisting people in building amazing frontends with a touch of backend development to incorporate essential functionalities. I also have a passion for designing unique products for websites, enabling businesses to enhance their strategic approaches. I am excited to apply my skills to various projects."
+        parag2: "I enjoy assisting people in building amazing frontends with a touch of backend development to incorporate essential functionalities. I also have a passion for designing unique products for websites, enabling businesses to enhance their strategic approaches. I am excited to apply my skills to various projects.",
+    }
+]
+
+export const frontend = [
+    {
+        tech: "./assets/html.svg",
+        alt: "HTML",
+    },
+    {
+        tech: "./assets/css.svg",
+        alt: "CSS",
+    },
+    {
+        tech: "./assets/javascript.svg",
+        alt: "JavaScript",
+    },
+    {
+        tech: "./assets/sass.svg",
+        alt: "Sass",
+    },
+    {
+        tech: "./assets/jquery.svg",
+        alt: "JQuery"
+    },
+    {
+        tech: "./assets/bootstrap.svg",
+        alt: "Bootstrap",
+    },
+    {
+        tech: "./assets/react.svg",
+        alt: "React",
+    },
+    {
+        tech: "./assets/tailwind.svg",
+        alt: "Tailwind",
+    },
+]
+
+export const backend = [
+    {
+        tech: "./assets/php.svg",
+        alt: "PHP"
+    },
+    {
+        tech: "./assets/mysql.svg",
+        alt: "MySQL"
+    },
+]
+
+export const tools = [
+    {
+        tech: "./assets/figma.svg",
+        alt: "Figma"
+    },
+    {
+        tech: "./assets/adobexd.svg",
+        alt: "Adobe XD"
+    },
+    {
+        tech: "./assets/git.svg",
+        alt: "Git"
+    },
+    {
+        tech: "./assets/github.svg",
+        alt: "GitHUB"
     },
 ]
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { aboutContent } from '../components/PageTemplate';
+import { aboutContent, frontend, backend, tools } from '../components/PageTemplate';
 
 import '../sass/About.scss';
 
@@ -11,13 +11,50 @@ const About = () => {
             aboutContent.map((about, index) =>(
             <div className='wrapper'>
                 <div className="left-content">
-                    <span className='francisbeam'></span>
-                    {/* <img src="./assets/francisbeamsantander.jpg" alt="" /> */}
+                    <div role='img' className='francisbeam'>
+                        <img src="../assets/profile.svg" alt="Photo of Francis Beam" draggable="false"/>
+                    </div>
                 </div>
                 <div className="right-content">
                     <h1>ABOUT ME</h1>
                     <p>{about.parag1}</p>
                     <p>{about.parag2}</p>
+                    <div className='skills-tech'>
+                        <h4>Frontend Development</h4>
+                        <ul className='skill-items'>
+                            {
+                                frontend.map((stack, index) =>(
+                                <li key={index}>
+                                    <img src={stack.tech} alt={stack.alt} width={50} draggable="false" />
+                                </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                    <div className='skills-tech'>
+                        <h4>Backend Development</h4>
+                        <ul className='skill-items'>
+                            {
+                                backend.map((stack, index) =>(
+                                <li key={index}>
+                                    <img src={stack.tech} alt={stack.alt} width={50} draggable="false" />
+                                </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                    <div className='skills-tech'>
+                        <h4>Tools</h4>
+                        <ul className='skill-items'>
+                            {
+                                tools.map((stack, index) =>(
+                                <li key={index}>
+                                    <img src={stack.tech} alt={stack.alt} width={50} draggable="false" />
+                                </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
                 </div>
             </div>
             ))
