74e7758f63e9b43fec8ce85d476cbed6ebc66cbe
Francis Beam Santander <santanderfrancisbeam@gmail.com>
2023-07-11 01:23:19 +0800

    changed basename

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ const AppLayout = () => {
 
 
 ReactDOM.render(
-  <Router basename='https://francebeam.github.io/francisbeamsantander/'>
+  <Router basename='/francisbeamsantander'>
     <AppLayout />
   </Router>,
   document.getElementById('root')
